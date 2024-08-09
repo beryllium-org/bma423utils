7859589c51bd3351edfc6ee89b72a98e545a3319
Bill Sideris <bill88t@feline.gr>
2024-08-09 03:10:36 +0300

    i2c bus selection

--- files/bma423axis.py
+++ files/bma423axis.py
@@ -1,8 +1,18 @@
 rename_process("bma423axis")
 vr("opts", be.api.xarg())
-vr("i2c", be.devices["i2c"][0])
+be.api.setvar("return", "1")
 if "i" in vr("opts")["o"]:
+    vr("busn", 0)
+    if vr("opts")["o"]["i"] is not None:
+        try:
+            vr("nbus", vr("opts")["o"]["i"])
+            if not vr("nbus").startswith("/dev/i2c"):
+                raise RuntimeError
+            vr("busn", int(vr("opts")["o"]["i"][-1:]))
+        except:
+            term.write("Could not parse node, using default.")
     try:
+        vr("i2c", be.devices["i2c"][vr("busn")])
         from bma423 import BMA423
         vr("bma", BMA423(vr("i2c")))
         be.based.run("mknod BMA423")
@@ -37,8 +47,10 @@ if "i" in vr("opts")["o"]:
             pass
     be.api.setvar("return", "0")
 elif "d" in vr("opts")["o"]:
-    be.based.run("rmnod BMA423_0")
-    be.based.run("rmnod temp0")
-    be.api.setvar("return", "0")
+    if vr("dev") is not None and vr("dev").startswith("/dev/BMA423_"):
+        be.based.run("rmnod " + vr("dev")[5:])
+        be.api.setvar("return", "0")
+    else:
+        term.write("Invalid device node!")
 else:
     term.write("Usage:\n    axp2101pmic -i\n    axp2101pmic -d")
